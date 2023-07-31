349900ac9560630bfa0917067f500e74242ff692
Benjamin Millant <bemi@itu.dk>
2023-08-01 00:07:04 +0200

    Some simple testing with print statements. Think we need to handle an empty board in order to do propor testing of whether the findword function works or now

--- ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/Program.fs
@@ -35,9 +35,9 @@ let main argv =
     //    let board      = ScrabbleUtil.HoleBoard.holeBoard ()
     //    let board      = ScrabbleUtil.InfiniteHoleBoard.infiniteHoleBoard ()
 
-    let words = readLines "../../../Dictionaries/English.txt"
+    //let words = readLines "../../../Dictionaries/English.txt"
     //B ⬇️ W ^
-    //let words = readLines (System.IO.Path.Combine("Dictionaries", "English.txt"))
+    let words = readLines (System.IO.Path.Combine("Dictionaries", "English.txt"))
 
     let handSize = 7u
     let timeout = None
