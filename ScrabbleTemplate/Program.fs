47d0cec6db06d76bd0afaec17c6e81cbbcc1fddb
Benjamin Millant <bemi@itu.dk>
2023-07-31 11:03:30 +0200

    Added file path reader for Ben compatability

--- ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/Program.fs
@@ -10,10 +10,10 @@ let readLines filePath = System.IO.File.ReadLines(filePath)
 
 let spawnMultiples name dict bot =
     let rec aux =
-        function 
+        function
         | 0 -> []
-        | x -> (sprintf "%s%d" name x, dict, bot)::aux (x - 1)
-   
+        | x -> (sprintf "%s%d" name x, dict, bot) :: aux (x - 1)
+
     aux >> List.rev
 
 [<EntryPoint>]
@@ -24,30 +24,32 @@ let main argv =
     System.Console.ForegroundColor <- System.ConsoleColor.Black
     System.Console.Clear()
 
-    let board        = ScrabbleUtil.StandardBoard.standardBoard ()
-//    let board      = ScrabbleUtil.InfiniteBoard.infiniteBoard ()
+    let board = ScrabbleUtil.StandardBoard.standardBoard ()
+    //    let board      = ScrabbleUtil.InfiniteBoard.infiniteBoard ()
 
-//    let board      = ScrabbleUtil.RandomBoard.randomBoard ()
-//    let board      = ScrabbleUtil.RandomBoard.randomBoardSeed (Some 42)
-//    let board      = ScrabbleUtil.InfiniteRandomBoard.infiniteRandomBoard ()
-//    let board      = ScrabbleUtil.InfiniteRandomBoard.infiniteRandomBoardSeed (Some 42)
+    //    let board      = ScrabbleUtil.RandomBoard.randomBoard ()
+    //    let board      = ScrabbleUtil.RandomBoard.randomBoardSeed (Some 42)
+    //    let board      = ScrabbleUtil.InfiniteRandomBoard.infiniteRandomBoard ()
+    //    let board      = ScrabbleUtil.InfiniteRandomBoard.infiniteRandomBoardSeed (Some 42)
 
-//    let board      = ScrabbleUtil.HoleBoard.holeBoard ()
-//    let board      = ScrabbleUtil.InfiniteHoleBoard.infiniteHoleBoard ()
+    //    let board      = ScrabbleUtil.HoleBoard.holeBoard ()
+    //    let board      = ScrabbleUtil.InfiniteHoleBoard.infiniteHoleBoard ()
 
-    let words     = readLines "../../../Dictionaries/English.txt"
+    let words = readLines "../../../Dictionaries/English.txt"
+    //B ⬇️ W ^
+    //let words = readLines (System.IO.Path.Combine("Dictionaries", "English.txt"))
 
-    let handSize   = 7u
-    let timeout    = None
-    let tiles      = ScrabbleUtil.English.tiles 1u
-    let seed       = None
-    let port       = 13001
+    let handSize = 7u
+    let timeout = None
+    let tiles = ScrabbleUtil.English.tiles 1u
+    let seed = None
+    let port = 13001
 
     let dictAPI =
         // Uncomment if you have implemented a dictionary. last element None if you have not implemented a GADDAG
-        Some (Dictionary.empty, Dictionary.insert, Dictionary.step, None) 
-        // None
-        
+        Some(Dictionary.empty, Dictionary.insert, Dictionary.step, None)
+    // None
+
     // Uncomment this line to call your client
     // let players    = [("Zyzzyva", Zyzzyva.Scrabble.startGame)]
     let (dictionary, time) =
@@ -55,12 +57,11 @@ let main argv =
 
     // let players = spawnMultiples "OxyphenButazone" dictionary Oxyphenbutazone.Scrabble.startGame 2
     let players = spawnMultiples "Zyzzyva" dictionary Zyzzyva.Scrabble.startGame 2
-    
 
-    do ScrabbleServer.Comm.startGame 
-          board dictionary handSize timeout tiles seed port players
-    
+
+    do ScrabbleServer.Comm.startGame board dictionary handSize timeout tiles seed port players
+
     ScrabbleUtil.DebugPrint.forcePrint ("Server has terminated. Press Enter to exit program.\n")
-    System.Console.ReadLine () |> ignore
+    System.Console.ReadLine() |> ignore
 
     0
