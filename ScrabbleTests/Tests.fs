0f8eaae744bed873fd32852ac7744e099b9ff34d
William Skou Heidemann <Williamskou2227@gmail.com>
2023-08-01 11:25:12 +0200

    created board as map from coord to tile (id, (char, point)) in test file

--- ScrabbleTests/Tests.fs
+++ ScrabbleTests/Tests.fs
@@ -47,3 +47,24 @@ let ``gaddag of A has node count of 2`` () =
 // let ``hand of A`` () =
     // let handOfA = empty |> addSingle 1u
     // let tileMap = Map.empty |> Map.add 1u Set ('A', 1)
+    
+[<Fact>]
+let ``Board containing A has 1 tile`` () =
+    let centerCoord = (0,0)
+    let tileA = (1u, ('A', 1))
+    let placedTiles = Map.empty |> Map.add centerCoord tileA
+    Assert.True (placedTiles.Count = 1)
+    
+let fiveLetterWordCoordinates = [(0,0); (1,0); (2,0); (3,0); (4,0)]
+let tileH = (8u, ('H', 1))
+let tileE = (5u, ('E', 1))
+let tileL = (12u, ('L', 1))
+let tileO = (15u, ('O', 1))
+let helloTiles = [tileH; tileE; tileL; tileL; tileO]
+
+let helloOnBoard = List.zip fiveLetterWordCoordinates helloTiles |> Map.ofList // map from coord to tile
+
+[<Fact>]
+let ``Board containing HELLO has 5 tiles`` () =
+    Assert.True (helloOnBoard.Count = 5)
+    
